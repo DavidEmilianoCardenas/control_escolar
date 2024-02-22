17d307fce8536c9f17f372f00595e5fab588404c
David Emiliano <dcardenasmontesino@gmail.com>
2024-02-22 13:39:12 +0100

    añadir union a sql

--- controlEscolar/src/main/java/paur/davide/controlescolar/ControlEscolar.java
+++ controlEscolar/src/main/java/paur/davide/controlescolar/ControlEscolar.java
@@ -4,6 +4,15 @@
 
 package paur.davide.controlescolar;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mati
@@ -11,6 +20,42 @@ package paur.davide.controlescolar;
 public class ControlEscolar {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+            
+            try {
+                Properties props = new Properties();
+                props.load(new FileInputStream("config/db.properties"));
+                String username = props.getProperty("user");
+                String password = props.getProperty("password");
+                String bd_name = props.getProperty("bd");
+                
+                String host = "jdbc:mysql://localhost/bd1";
+                 try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+
+                    Connection conexion = DriverManager.getConnection(host, username, password);
+
+                    closeConnection(conexion);
+                } catch (SQLException | ClassNotFoundException ex) {
+                    System.out.println("Error en la conexion de la base de datos");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+    
+    
+     
+    public static void closeConnection(Connection con)
+    {
+        try
+        {
+            con.close();
+            JOptionPane.showMessageDialog(null, "Se ha finalizado la conexión con el servidor");
+        }catch (SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, "No se ha podido finalizar la conexión con el servidor");
+
+        }
     }
 }
